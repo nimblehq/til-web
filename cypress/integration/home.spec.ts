692bdb8564ecf6a8cbff861471886cd8b8f0d5d2
Hoang Mirs <hoang.mirs@gmail.com>
2022-06-08 10:57:57 +0700

    [#11] Add UI tests

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,6 +1,7 @@
 const homeDataTestIds = {
   heading: 'home-heading',
   postList: 'post-list',
+  postLink: 'post-card__link',
 };
 
 describe('Home screen', () => {
@@ -11,5 +12,17 @@ describe('Home screen', () => {
     cy.url().should('include', '/');
     cy.findByTestId(homeDataTestIds.heading).contains('TIL');
     cy.findByTestId(homeDataTestIds.postList).should('exist');
+  });
+
+  context('when clicking on a post', () => {
+    it('navigates to the post page', () => {
+      // Start from the index page
+      cy.visit('/');
+
+      // Click on the first post
+      cy.findByTestId(homeDataTestIds.postLink).first().click();
+
+      cy.url().should('include', '/posts/');
+    });
   });
 });
