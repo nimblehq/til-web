61620021caaa3a130cf6ca12fe9420a17ce05e2c
tyrro <rajib.das.shuva@gmail.com>
2022-06-10 16:12:46 +0600

    [#21] chore(tests): Add tests for posts/[slug] page

--- src/pages/posts/[slug].test.tsx
+++ src/pages/posts/[slug].test.tsx
@@ -1,19 +1,19 @@
 import { render, screen } from '@testing-library/react';
 
-import { BASIC_FIELDS, getAllPosts } from 'lib/post';
+import { EXTENDED_FIELDS, getAllPosts } from 'lib/post';
 
 import PostDetails, { postDetailsTestIds } from './[slug].page';
 
 describe('PostDetails', () => {
-  it('renders the title', () => {
-    const posts = getAllPosts(BASIC_FIELDS);
+  it('renders a home link', () => {
+    const posts = getAllPosts(EXTENDED_FIELDS);
     const post = posts[0];
 
     render(<PostDetails post={post} />);
 
-    const title = screen.getByTestId(postDetailsTestIds.title);
+    const homePageLink = screen.getByTestId(postDetailsTestIds.homePageLink);
 
-    expect(title).toBeVisible();
-    expect(title).toHaveTextContent(post.title);
+    expect(homePageLink).toBeVisible();
+    expect(homePageLink).toHaveTextContent('TIL');
   });
 });
