147580fbec342b9cc4e1c45284b63704803a70f9
Hoang Mirs <hoang.mirs@gmail.com>
2022-07-04 17:45:37 +0700

    [#23] Remove fixed data-test-id

--- src/components/Image/index.test.tsx
+++ src/components/Image/index.test.tsx
@@ -1,13 +1,22 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Image from '.';
 
 describe('Image', () => {
   it('renders the image properly', () => {
-    const { container } = render(
-      <Image src="https://picsum.photos/200/300" alt="test" layout="fill" />
+    render(
+      <Image
+        src="https://picsum.photos/200/300"
+        alt="Image"
+        data-test-id="image"
+        layout="fill"
+      />
     );
 
-    expect(container).toMatchSnapshot();
+    const imageElement = screen.getByTestId('image');
+
+    expect(imageElement).toBeVisible();
+    expect(imageElement).toHaveAttribute('src');
+    expect(imageElement).toHaveAttribute('alt');
   });
 });
