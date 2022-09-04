4596d977b281a58ebd09ef6b31b3a7f5d0004a00
Hoang Mirs <hoang.mirs@gmail.com>
2022-09-04 21:20:31 +0700

    [#45][Frontend] As a user, I can see the post's UI has been similarly to the blog website

--- src/helpers/url.test.ts
+++ src/helpers/url.test.ts
@@ -1,4 +1,4 @@
-import { isValidUrl } from './url';
+import { getAssetUrl, isValidUrl } from './url';
 
 describe('URL helper', () => {
   describe('isValidUrl', () => {
@@ -19,6 +19,60 @@ describe('URL helper', () => {
         const isValid = isValidUrl(url);
 
         expect(isValid).toBe(false);
+      });
+    });
+  });
+
+  describe('getAssetUrl', () => {
+    describe('given a valid URL', () => {
+      it('returns the URL', () => {
+        const url = 'https://example.com';
+
+        const assetUrl = getAssetUrl(url);
+
+        expect(assetUrl).toBe(url);
+      });
+    });
+
+    describe('given a path', () => {
+      const originalEnv = process.env;
+
+      afterEach(() => {
+        process.env = originalEnv;
+      });
+
+      describe('when in production', () => {
+        it('returns the path', () => {
+          process.env = { ...originalEnv, NODE_ENV: 'production' };
+
+          const path = '/path/to/asset';
+
+          const assetUrl = getAssetUrl(path);
+
+          expect(assetUrl).toBe(path);
+        });
+      });
+
+      describe('when NOT in production', () => {
+        it('returns the path prefixed with the base path', () => {
+          process.env = { ...originalEnv, NODE_ENV: 'development' };
+
+          const path = '/path/to/asset';
+
+          const assetUrl = getAssetUrl(path);
+
+          expect(assetUrl).toBe(`${process.env.NEXT_PUBLIC_BASE_PATH}${path}`);
+        });
+      });
+    });
+
+    describe('given an INVALID path', () => {
+      it('returns an empty string', () => {
+        const path = 123;
+
+        const assetUrl = getAssetUrl(path);
+
+        expect(assetUrl).toBe('');
       });
     });
   });
--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,11 +1,48 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+import { getAssetUrl } from 'helpers/url';
+
 function Document() {
+  const renderFontStyle = () => (
+    <style
+      dangerouslySetInnerHTML={{
+        __html: `
+          @font-face {
+            font-family: 'Neuzeit S';
+            font-weight: 500;
+            src:
+              url('${getAssetUrl('/til/assets/fonts/NeuzeitS-Book.ttf')}'),
+              url('${getAssetUrl(
+                '/til/assets/fonts/NeuzeitS-Book.woff2'
+              )}') format('woff2'),
+              url('${getAssetUrl('/fonts/NeuzeitS-Book.woff')}') format('woff');
+            font-display: swap;
+          }
+
+          @font-face {
+            font-family: 'Neuzeit S';
+            font-weight: bold;
+            src:
+              url('${getAssetUrl('/til/assets/fonts/NeuzeitS-BookHeavy.ttf')}'),
+              url('${getAssetUrl(
+                '/til/assets/fonts/NeuzeitS-BookHeavy.woff2'
+              )}') format('woff2'),
+              url('${getAssetUrl(
+                '/til/assets/fonts/NeuzeitS-BookHeavy.woff'
+              )}') format('woff');
+            font-display: swap;
+          }
+        `,
+      }}
+    />
+  );
+
   return (
     <Html lang="en">
       <Head>
         <meta name="description" content="What Nimble has learned today" />
         <link rel="icon" href="/til/favicon.ico" />
+        {renderFontStyle()}
       </Head>
       <body>
         <Main />
